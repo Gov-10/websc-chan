30e1c4ab71c735a7fde8667092b173738707b11f
Govind <govindsys1008@gmail.com>
2025-07-29 10:32:32 +0530

    added requirements.txt file

--- chatbot/chatbot/consumers.py
+++ chatbot/chatbot/consumers.py
@@ -3,10 +3,17 @@ import json
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai
+from asgiref.sync import sync_to_async
 load_dotenv()
 gemini_api_key = os.getenv('GEMINI_API_KEY')
 genai.configure(api_key=gemini_api_key)
 model = genai.GenerativeModel("gemini-1.5-flash")
+
+@sync_to_async
+def generate_content(message):
+    response = model.generate_content(message)
+    reply = response.text.strip()
+    return reply
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -14,8 +21,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data = json.loads(text_data)
         message = data['message']
-        response = model.generate_content(message)
-        reply = response.text.strip()
+        reply = await generate_content(message)
         await self.send(text_data=json.dumps(
             {
                 'message' : f"Chatbot: {reply}"
